0e0c85bc16629bc7ef788a4e53abbc96e99a4e9b
alfredo <star@stars-iMac.local>
2016-11-24 17:38:59 +0100

    Hot Loader Fix

--- app/containers/Application.js
+++ app/containers/Application.js
@@ -26,10 +26,11 @@ export default class Application extends React.Component {
   render () {
     return (
       <Provider store={ this.props.store }>
-        <Router history={ this.props.history }> //add this when pushing to prod  onUpdate={() => this.handlePageView()}
+        <Router history={ this.props.history }> 
           {routes}
         </Router>
       </Provider>
     )
   }
 }
+//add this when pushing to prod  onUpdate={() => this.handlePageView()}
