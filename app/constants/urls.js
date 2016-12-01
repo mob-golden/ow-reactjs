b7be69fadbbe460bbd52bf61398149672fb29d84
alfredo <alfredo.golden@hotmail.com>
2016-12-01 05:50:44 +0100

    SSL remove

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -13,18 +13,18 @@ export const RIOT_SPRITES_URL = `${RIOT_API_ROOT}/img/sprite`;
 // export const CS_API_ROOT = 'http://champion-select-api-prod.herokuapp.com';
 
 // production
-export const CS_API_ROOT = '//api.championselect.net';
+export const CS_API_ROOT = 'http://api.championselect.net';
 
 export const CS_CHAMPION_URL = `${CS_API_ROOT}/champion`;
 export const CS_COUNTER_TIPS_URL = `${CS_API_ROOT}/countertips`;
 export const CS_COMMENTS_URL = `${CS_API_ROOT}/comments`;
 
-export const SOLOMID_AUTH_ROOT = '//auth.solomid.net';
+export const SOLOMID_AUTH_ROOT = 'http://auth.solomid.net';
 // export const SOLOMID_AUTH_ROOT = 'http://localhost:5000';
 export const SOLOMID_AUTH_SIGN_IN_URL = `${SOLOMID_AUTH_ROOT}/token`;
 export const SOLOMID_AUTH_SIGN_UP_URL = `${SOLOMID_AUTH_ROOT}/user/create`;
 export const SOLOMID_AUTH_FORGOT_PASS_URL = `${SOLOMID_AUTH_ROOT}/user/forgot/password`;
 export const SOLOMID_AUTH_RESET_PASS_URL = `${SOLOMID_AUTH_ROOT}/user/update/password`;
 
-export const CHAMPION_GG_ROOT = '//api.champion.gg';
+export const CHAMPION_GG_ROOT = 'http://api.champion.gg';
 export const CHAMPION_GG_MOST_PLAYED_URL = `${CHAMPION_GG_ROOT}/stats/champs/mostPlayed`;
