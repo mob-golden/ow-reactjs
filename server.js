c82120f899ca6f509339ac432353d1c631017857
alfredo <alfredo.golden@hotmail.com>
2016-12-06 18:11:52 +0100

    Node update

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var qs = require('querystring');
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./app/containers/Application');
-var throng = require('throng');
 
 var urls = require('./app/constants/urls');
 
@@ -19,115 +18,102 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
 var staticPath = path.join(__dirname, '/dist');
-const WORKERS = process.env.WEB_CONCURRENCY || 1;
 const S_IN_YR = 31536000;
 var permaNews = {};
-const start = () => {
-  app.use(express.static(staticPath, { maxAge: S_IN_YR }));
-  app.use(compression());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
 
-  app.get('/', handleRender);
+app.use(express.static(staticPath, { maxAge: S_IN_YR }));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-  app.post('/signin', function (req, res) {
-    const {
-      email,
-      password
-    } = req.body;
+app.get('/', handleRender);
 
-    const data = qs.stringify({
-      'grant_type': 'password',
-      email,
-      password
-    });
-    fetch(`${urls.SOLOMID_AUTH_SIGN_IN_URL}`, {
-      body: data,
-      headers: constructHeaders(data),
-      method: 'POST'
+app.post('/signin', function (req, res) {
+  const {
+    email,
+    password
+  } = req.body;
+
+  const data = qs.stringify({
+    'grant_type': 'password',
+    email,
+    password
+  });
+  fetch(`${urls.SOLOMID_AUTH_SIGN_IN_URL}`, {
+    body: data,
+    headers: constructHeaders(data),
+    method: 'POST'
+  })
+    .then(response => response.json())
+    .then(json => {
+      res.json(json);
     })
-      .then(response => response.json())
-      .then(json => {
-        res.json(json);
-      })
-      .catch(error => {
-        res.json(error);
-      });
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+app.post('/signup', function (req, res) {
+  const {
+    username,
+    email,
+    password
+  } = req.body;
+
+  const data = qs.stringify({
+    username,
+    email,
+    password
   });
 
-  app.post('/signup', function (req, res) {
-    const {
-      username,
-      email,
-      password
-    } = req.body;
-
-    const data = qs.stringify({
-      username,
-      email,
-      password
-    });
-
-    fetch(`${urls.SOLOMID_AUTH_SIGN_UP_URL}`, {
-      body: data,
-      headers: constructHeaders(data),
-      method: 'POST'
+  fetch(`${urls.SOLOMID_AUTH_SIGN_UP_URL}`, {
+    body: data,
+    headers: constructHeaders(data),
+    method: 'POST'
+  })
+    .then(response => response.json())
+    .then(json => {
+      res.json(json);
     })
-      .then(response => response.json())
-      .then(json => {
-        res.json(json);
-      })
-      .catch(error => {
-        res.json(error);
-      });
-  });
+    .catch(error => {
+      res.json(error);
+    });
+});
 
 
-  function handleRender(req, res) {
-    match({ routes, location: req.url }, function(error, redirectLocation, renderProps) {
-      if (error) {
-        console.log("[match]: error", error);
-        res.status(500).send(error.message)
-      } else if (redirectLocation) {
-        console.log("[match]: redirectLocation", redirectLocation);
-        res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-      } else if (renderProps) {
-        if (typeof renderProps.routes[1] !== 'undefined' && renderProps.routes[1].status === 404) {
-          res.status(404).sendFile(path.join(__dirname, '/dist/index.html'));
-        } else {
-          res.sendFile(path.join(__dirname, '/dist/index.html'));
-        }
-    } else {
-        console.log("[match]: Not found");
-        res.status(404).send('Not found');
+function handleRender(req, res) {
+  match({ routes, location: req.url }, function(error, redirectLocation, renderProps) {
+    if (error) {
+      console.log("[match]: error", error);
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      console.log("[match]: redirectLocation", redirectLocation);
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      if (typeof renderProps.routes[1] !== 'undefined' && renderProps.routes[1].status === 404) {
+        res.status(404).sendFile(path.join(__dirname, '/dist/index.html'));
+      } else {
+        res.sendFile(path.join(__dirname, '/dist/index.html'));
       }
-    });
-  }
-
-  app.listen(port, function() {
-      console.log('listening')
+  } else {
+      console.log("[match]: Not found");
+      res.status(404).send('Not found');
+    }
   });
-
-  function constructHeaders (data) {
-    const credentials = new Buffer(`${process.env.AUTH_ID}:${process.env.AUTH_SECRET}`).toString('base64');
-
-    return new Headers({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': data.length,
-      'Authorization': `Basic ${credentials}`
-    });
-  }
-
-};
-
-if (process.env.NODE_ENV === 'production') {
-  throng({
-    lifetime: Infinity,
-    start,
-    workers: WORKERS,
-  });
-} else {
-  start();
 }
+
+function constructHeaders (data) {
+  const credentials = new Buffer(`${process.env.AUTH_ID}:${process.env.AUTH_SECRET}`).toString('base64');
+
+  return new Headers({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': data.length,
+    'Authorization': `Basic ${credentials}`
+  });
+}
+
+app.listen(port, function() {
+  	console.log('listening')
+});
