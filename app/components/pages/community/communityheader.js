fa3a2aea3bf8bdd15cb9615114d73d6b977ba906
Vincent <vincent.oddrepublic@gmail.com>
2017-01-08 20:56:12 -0800

    Added scroll to bottom on discussion

--- app/components/pages/community/communityheader.js
+++ app/components/pages/community/communityheader.js
@@ -4,6 +4,12 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 class CommunityHeader extends Component {
+  startDiscussion (e) {
+    $('html, body').animate({ 
+       scrollTop: $(document).height()-$(window).height()}, 
+       600
+    );  
+  }
   render() {
     return (
       <div className="row">
@@ -11,7 +17,10 @@ class CommunityHeader extends Component {
           <div className="os-content-top os-comm-header">
             <p className="os-white os-font-size-30 os-comm-welcome">Welcome to the SoloMid</p>
             <p className="os-white os-font-size-45 os-comm-big">OVERWATCH COMMUNITY</p>
-            <button className="btn btn-warning btn-comm">
+            <button 
+              className="btn btn-warning btn-comm"
+              onClick={(e) => {this.startDiscussion(e)}}
+            >
               <small>START A DISCUSSION</small>
             </button>
           </div>
