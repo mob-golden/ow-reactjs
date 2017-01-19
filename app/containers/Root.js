98ba3fc702bfdbdd2d00912ce1ce3cfa1bbaae8a
Alfredo <alfredo.golden@hotmail.com>
2017-01-19 19:10:40 +0100

    delete unnecessary package

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { RouteTransition } from 'react-router-transition';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import { Component, PropTypes } from 'react';
