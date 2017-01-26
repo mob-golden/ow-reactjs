02917adeeb76979952c2e3acd8327359c4ae94b0
AdilVirani <adil@solomid.net>
2017-01-26 01:52:37 -0600

    enable cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('babel-register');
 require('dotenv').config();
 var path = require('path');
+var cors = require('cors');
 var express = require('express');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
@@ -26,6 +27,7 @@ var port = process.env.PORT || 3000;
 var staticPath = isDevelopment ? path.join(__dirname, '/app') : path.join(__dirname, '/dist');
 var overwatchHost = process.env.OVERWATCH_HOST || "https://overwatch-select-api-prod.herokuapp.com";
 const S_IN_YR = 31536000;
+app.use(cors());
 app.use(express.static(staticPath, { maxAge: S_IN_YR }));
 app.use(session({
     store: new RedisStore({
